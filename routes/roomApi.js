c3451e11b36826acff51a0466223f5b44bd9687d
LoayTarek5 <loaytareq44@gmail.com>
2025-09-10 20:19:11 +0300

    refactor: bind room controller methods to maintain context in API routes

--- routes/roomApi.js
+++ routes/roomApi.js
@@ -4,9 +4,35 @@ import roomController from "../controllers/roomController.js";
 
 const router = express.Router();
 
-router.post("/create", requireAuth, roomController.createRoom);
-router.post('/join/:roomCode', requireAuth, roomController.joinRoom);
-router.post('/:roomId/leave', requireAuth, roomController.exitRoom);
-router.get("/:roomId", requireAuth, roomController.getRoomDetails);
+router.post(
+  "/create",
+  requireAuth,
+  roomController.createRoom.bind(roomController)
+);
+router.post(
+  "/:roomId/invite",
+  requireAuth,
+  roomController.inviteFriends.bind(roomController)
+);
+router.post(
+  "/join/:roomCode",
+  requireAuth,
+  roomController.joinRoom.bind(roomController)
+);
+router.post(
+  "/:roomId/leave",
+  requireAuth,
+  roomController.exitRoom.bind(roomController)
+);
+router.get(
+  "/:roomId",
+  requireAuth,
+  roomController.getRoomDetails.bind(roomController)
+);
+router.get(
+  "/:roomId/available-friends",
+  requireAuth,
+  roomController.getAvailableFriends.bind(roomController)
+);
 
 export default router;
